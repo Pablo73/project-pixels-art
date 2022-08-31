427947a67a971e73716789f30293980ffabdcfc6
Pablo Dominguez <pasaxo@gmail.com>
2022-08-31 15:18:51 -0300

    Requisito 1

---
+++ script.js
@@ -0,0 +1,5 @@
+const positionBody = document.body;
+const createTitle = document.createElement('h1');
+createTitle.innerHTML = 'Paleta de Cores';
+createTitle.id = 'title';
+positionBody.appendChild(createTitle);
